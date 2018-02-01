5c12db19018dcbfb6497af0cf135280f99396480
xfmeng17 <xfmeng17@foxmail.com>
2018-02-01 11:18:14 +0800

    init coursera

---
+++ pl/hw4/hw4.rkt
@@ -0,0 +1,97 @@
+
+#lang racket
+
+(provide (all-defined-out)) ;; so we can put tests in a second file
+
+;; put your code below
+
+;; 1. sequence
+(define (sequence low high stride)
+  (if (> low high)
+      null
+      (cons low (sequence (+ low stride) high stride))))
+
+;; 2. string-append-map
+(define (string-append-map xs suffix)
+  (map (lambda (x) (string-append x suffix)) xs))
+
+;; 3. list-nth-mod
+(define (list-nth-mod xs n)
+  (cond [(negative? n) (error "list-nth-mod: negative number")]
+        [(null? xs) (error "list-nth-mod: empty list")]
+        [#t (car (list-tail xs (remainder n (length xs))))]))
+
+;; 4. stream-for-n-steps
+(define (stream-for-n-steps s n)
+  (if (= 0 n)
+      null
+      (cons (car (s)) (stream-for-n-steps (cdr (s)) (- n 1)))))
+
+;; 5. funny-number-stream
+(define funny-number-stream
+  (letrec ([f (lambda (x)
+                (if (= 0 (remainder x 5))
+                    (cons (- 0 x) (lambda () (f (+ x 1))))
+                    (cons x (lambda () (f (+ x 1))))))])
+    (lambda () (f 1))))
+
+;; 6. dan-then-dog
+(define dan-then-dog
+  (letrec ([f (lambda (x)
+                (if (= 0 x)
+                    (cons "dan.jpg" (lambda () (f (+ x 1))))
+                    (cons "dog.jpg" (lambda () (f (- x 1))))))])
+    (lambda () (f 0))))
+
+;; 7.steam-add-zero
+(define (stream-add-zero s)
+  (lambda () (cons (cons 0 (car (s))) (stream-add-zero (cdr (s))))))
+
+;; 8. cycle-lists
+(define (cycle-lists xs ys)
+  (letrec ([f (lambda (n) (cons
+                           (cons (list-nth-mod xs n) (list-nth-mod ys n))
+                           (lambda () (f (+ n 1)))))])
+    (lambda () (f 0))))
+
+;; 9. vector-assoc
+(define (vector-assoc v vec)
+  (letrec ([f (lambda (p)
+             (if (>= p (vector-length vec))
+                 #f
+                 (if (pair? (vector-ref vec p))
+                     (if (equal? v (car (vector-ref vec p)))
+                         (vector-ref vec p)
+                         (f (+ p 1)))
+                     (f (+ p 1)))))])
+    (f 0)))
+
+;; 10. cached-assoc
+(define (cached-assoc xs n)
+  (letrec ([memo (make-vector n #f)]
+           [next 0]          
+           [f (lambda (v)
+                (let ([ans (vector-assoc v memo)])
+                  (if ans
+                      ans
+                      (let ([new-ans (assoc v xs)])
+                        (begin
+                          (vector-set! memo next new-ans)
+                          (set! next (remainder (+ next 1) n))
+                          new-ans)))))])
+    f))
+
+;; 11. macro
+(define-syntax while-less
+  (syntax-rules (do)
+    [(while-less e1 do e2)
+    (letrec ([x e1]
+             [f (lambda ()
+               (if (< e2 x)
+                   (f)
+                   #t))])
+      (f))]))
+  
+;; test for thunk
+; (define (test1 x) (lambda() (print x)))
+; (define (test2 x) (print x))
