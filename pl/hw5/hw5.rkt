5b4d8d32cb3faa93c83e54a6ff3fe9c11eef3d96
xfmeng17 <xfmeng17@foxmail.com>
2018-05-06 15:29:08 +0800

    finish int? and ifgreater?

--- pl/hw5/hw5.rkt
+++ pl/hw5/hw5.rkt
@@ -64,6 +64,18 @@
                        (int-num v2)))
                (error "MUPL addition applied to non-number")))]
         ;; CHANGE add more cases here
+        ;; [(int? e) (int-num e)]
+        [(ifgreater? e)
+         (print "hi")
+         (let ([v1 (eval-under-env (ifgreater-e1 e) env)]
+               [v2 (eval-under-env (ifgreater-e2 e) env)]
+               [v3 (eval-under-env (ifgreater-e3 e) env)]
+               [v4 (eval-under-env (ifgreater-e4 e) env)])
+           (if (and (int? v1) (int? v2))
+               (if (> (int-num v1) (int-num v2))
+                   v3
+                   v4)
+               (error "MUPL ifgreater applied to non-number")))]
         [#t (error (format "bad MUPL expression: ~v" e))]))
 
 ;; Do NOT change
